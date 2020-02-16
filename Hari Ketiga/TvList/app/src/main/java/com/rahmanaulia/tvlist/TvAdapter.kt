e71ff7ea7a1aa23737a8aed68999d563bd441593
rhmn68 <auliar68@gmail.com>
2020-02-16 17:45:12 +0700

    add tvlist

--- Hari Ketiga/TvList/app/src/main/java/com/rahmanaulia/tvlist/TvAdapter.kt
+++ Hari Ketiga/TvList/app/src/main/java/com/rahmanaulia/tvlist/TvAdapter.kt
@@ -1,5 +1,23 @@
 package com.rahmanaulia.tvlist
 
-class TvAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
+class TvAdapter(private val context: Context): RecyclerView.Adapter<TvAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
