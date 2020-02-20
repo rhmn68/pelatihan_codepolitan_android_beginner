8ef419bcda90792f5a836497dc0776e7353fec41
rhmn68 <auliar68@gmail.com>
2020-02-21 05:25:48 +0700

    add starter project hari kelima

--- Hari Keempat/ContactApps/app/src/main/java/com/rahmanaulia/contactapps/model/Contact.kt
+++
@@ -1,7 +0,0 @@
-package com.rahmanaulia.contactapps.model
-
-data class Contact(
-    var id: Int? = null,
-    var name: String? = null,
-    var phone: String? = null
-)
--- Hari Keempat/MyNotesApp/app/src/main/java/com/rahmanaulia/mynotesapp/model/Note.kt
+++
@@ -1,8 +0,0 @@
-package com.rahmanaulia.mynotesapp.model
-
-data class Note(
-    var id: Int? = null,
-    var title: String? = null,
-    var desc: String? = null,
-    var date: String? = null
-)
